93d0474c3b60e4e0ca1bde7154ff72c55fc5a2f7
mcamargo <mcamargo@DESENV28.SEDE.SAFEWEB.COM.BR>
2017-11-07 14:27:09 -0200

    Criação de Palestrante

---
+++ TrabalhoDeImplementacao/src/Palestrante.java
@@ -0,0 +1,42 @@
+
+public class Palestrante {
+	private String nome, email, curriculo;
+	int telefone;
+
+	public String getCurriculo() {
+		return curriculo;
+	}
+
+	public void setCurriculo(String curriculo) {
+		this.curriculo = curriculo;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return "Nome = [" + nome + "], Email = [" + email + "]";
+	}
+
+}
